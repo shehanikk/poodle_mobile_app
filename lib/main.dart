0d386b035894ddb932702e3579acedc84c8921ab
shehanikk <park.shehani.7@gmail.com>
2023-03-04 19:20:36 +0530

    splash screen made

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:poodle_mobie_application/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Home Page'),
+      home: Splash(),
     );
   }
 }
