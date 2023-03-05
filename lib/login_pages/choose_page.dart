ac60646b0aa9dc44d57284f26c3a4a388615f5ff
shehanikk <park.shehani.7@gmail.com>
2023-03-05 15:29:59 +0530

    choose page made

---
+++ lib/login_pages/choose_page.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class Choose extends StatefulWidget {
+  const Choose({Key? key}) : super(key: key);
+
+  @override
+  State<Choose> createState() => _ChooseState();
+}
+
+class _ChooseState extends State<Choose> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: GestureDetector(
+          child: Stack(
+            children: [
+              Align(
+                alignment: AlignmentDirectional(0, 0),
+                child: Image.asset(
+                  'assets/choose_page.png',
+                  width: 448.2,
+                  height: 1013.3,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Align(
+                alignment: AlignmentDirectional(0, -0.45),
+                child: Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(20, 0, 20, 0),
+                  child: Text(
+                    'The dog is a domesticated descendant of the wolf is so good',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'Inter',
+                      color: Colors.white,
+                      fontSize: 19,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: AlignmentDirectional(0, -0.27),
+                child: ElevatedButton(
+                  onPressed: () { },
+                  child: const Text('LOGIN'),
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    fixedSize: MaterialStateProperty.all(const Size(165.7, 44.1)),
+                    backgroundColor: MaterialStateProperty.all(Color(0xFF64C251)),
+                    foregroundColor: MaterialStateProperty.all(Colors.white),
+                    textStyle: MaterialStateProperty.all(
+                      const TextStyle(
+                        fontSize: 16,
+                        fontFamily: 'Inter',
+                        color: Colors.white,
+                        letterSpacing: 2,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: AlignmentDirectional(0, -0.13),
+                child: ElevatedButton(
+                  onPressed: () { },
+                  child: const Text('SIGN IN'),
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    fixedSize: MaterialStateProperty.all(const Size(165.7, 44.1)),
+                    backgroundColor: MaterialStateProperty.all(Color(0xFFE4E9EB)),
+                    foregroundColor: MaterialStateProperty.all(Colors.black38),
+                    textStyle: MaterialStateProperty.all(
+                      const TextStyle(
+                        fontSize: 16,
+                        fontFamily: 'Inter',
+                        letterSpacing: 2,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
